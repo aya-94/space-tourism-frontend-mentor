9873b39ac67e424598e92082938d031b4e9d22db
hoday <hodayamirr@gmail.com>
2023-09-29 16:35:41 +0100

    Home Page Styles Large screen

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -3,17 +3,20 @@ import { Outlet } from 'react-router-dom';
 import MainNavigation from '../components/MainNavigation';
 
 const RootLayout = () => {
-    const styles = {
-        overflowY: 'auto',
+    const rootStyles = {
+        flexGrow: 3
+    }
+
+    const mainStyles = {
         height: 'calc(100% - 136px)'
     }
     return (
-        <>
+        <div style={rootStyles}>
             <MainNavigation />
-            <main style={styles}>
+            <main style={mainStyles}>
                 <Outlet />
             </main>
-        </>
+        </div>
     )
 }
 
