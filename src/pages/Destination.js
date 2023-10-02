3a03d9ec436e57a85150fce483ca2ef2749fc80d
hoday <hodayamirr@gmail.com>
2023-10-02 18:55:41 +0100

    finish section destination

--- src/pages/Destination.js
+++ src/pages/Destination.js
@@ -6,45 +6,75 @@ import classes from './Destination.module.css';
 
 const DestinationPage = () => {
     const { destinations } = jsonData;
-    const [planetImg, setPlanetImg] = useState(destinations[0].images.png);
+    const [planetImg, setPlanetImg] = useState(classes.moon);
     const [planetTitle, setPlanetTitle] = useState(destinations[0].name);
     const [planetParagraph, setPlanetParagraph] = useState(destinations[0].description);
     const [planetDistance, setPlanetDistance] = useState(destinations[0].distance);
     const [planetTravelTime, setPlanetTravelTime] = useState(destinations[0].travel);
+    const [active, setActive] = useState(0);
+
+    const HandleActive = (num) => {
+        setActive(num);
+        setPlanetTitle(destinations[num].name);
+        setPlanetParagraph(destinations[num].description);
+        setPlanetDistance(destinations[num].distance);
+        setPlanetTravelTime(destinations[num].travel);
+
+        if (num === 0) {
+            setPlanetImg(classes.moon);
+        } else if (num === 1) {
+            setPlanetImg(classes.mars);
+        } else if (num === 2) {
+            setPlanetImg(classes.europa);
+        } else if (num === 3) {
+            setPlanetImg(classes.titan);
+        }
+    }
 
     return ( 
-        <>
+        <div className={classes.destinationContainer}>
             <h3><span>01</span>PICK YOUR DESTINATION</h3>
             <div className={classes.container}>
-                <div className={classes.image}></div>
+                <div className={planetImg}></div>
                 <div className={classes.content}>
-                    <div className="planets">
+                    <div className={classes.planets}>
                         <nav>
                             <ul>
-                                <li>MOON</li>
-                                <li>MARS</li>
-                                <li>EUROPA</li>
-                                <li>TITAN</li>
+                                <li 
+                                    onClick={() => HandleActive(0)}
+                                    className={active === 0 ? classes.active : undefined}
+                                    >MOON</li>
+                                <li 
+                                    onClick={() => HandleActive(1)}
+                                    className={active === 1 ? classes.active : undefined}
+                                    >MARS</li>
+                                <li 
+                                    onClick={() => HandleActive(2)}
+                                    className={active === 2 ? classes.active : undefined}
+                                    >EUROPA</li>
+                                <li 
+                                    onClick={() => HandleActive(3)}
+                                    className={active === 3 ? classes.active : undefined}
+                                    >TITAN</li>
                             </ul>
                         </nav>
                     </div>
                     <h1>{planetTitle}</h1>
                     <p>{planetParagraph}</p>
-                    <span></span>
-                    <div className="info">
-                        <div className="distance">
+                    <div className={classes.line}></div>
+                    <div className={classes.info}>
+                        <div>
                             <h4>AVG. DISTANCE</h4>
                             <p>{planetDistance}</p>
                         </div>
-                        <div className="travel-time">
+                        <div>
                             <h4>EST. TRAVEL TIME</h4>
                             <p>{planetTravelTime}</p>
                         </div>
                     </div>
                 </div>
-            </div>
-            
-        </>
+            </div> 
+        </div>
      );
 }
  
