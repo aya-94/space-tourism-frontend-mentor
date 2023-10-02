d57b1411741d85589ccfbe4ebfcafb01e600b834
hoday <hodayamirr@gmail.com>
2023-10-02 18:55:59 +0100

    working on crew section styles

--- src/pages/Crew.js
+++ src/pages/Crew.js
@@ -1,6 +1,24 @@
+import classes from './Crew.module.css';
+
 const CrewPage = () => {
     return ( 
-        <h1>Crew Page!</h1>
+        <div className={classes.crewContainer}>
+            <div className={classes.container}>
+            <div className={classes.content}>
+            <h3><span>02</span>MEET YOUR CREW</h3>
+                <h2>COMMANDER</h2>
+                <h1>Douglas Hurley</h1>
+                <p>Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.</p>
+                <div className={classes.slider}>
+                    <div className={classes.member1}></div>
+                    <div className={classes.member2}></div>
+                    <div className={classes.member3}></div>
+                    <div className={classes.member4}></div>
+                </div>
+            </div>
+                <div className={classes.crewImg}></div>
+            </div>
+        </div>
      );
 }
  
