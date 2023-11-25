99e95076eb5488d17cfbeba4ec8f04e5d753d458
hoday <hodayamirr@gmail.com>
2023-11-25 21:23:18 +0100

    bones and some styles of technology page

--- src/pages/Technology.js
+++ src/pages/Technology.js
@@ -1,6 +1,58 @@
+import { useState } from 'react';
+
+import jsonData from '../data.json';
+import classes from './Technology.module.css';
+
 const TechnologyPage = () => {
+    const { technology } = jsonData;
+    const [technologyContainer, setTechnologyContainer] = useState(classes.launchContainer);
+    const [technologyName, setTechnologyName] = useState(technology[0].name);
+    const [technologyDescription, setTechnologyDescription] = useState(technology[0].description);
+    const [active, setActive] = useState(0);
+
+    const HandleActive = (num) => {
+        setActive(num);
+        setTechnologyName(technology[num].name);
+        setTechnologyDescription(technology[num].description);
+
+
+        if (num === 0) {
+            setTechnologyContainer(classes.launchContainer);
+        } else if (num === 1) {
+            setTechnologyContainer(classes.spaceportContainer);
+        } else if (num === 2) {
+            setTechnologyContainer(classes.capsuleContainer);
+        }
+    }
+
     return ( 
-        <h1>Technology Page!</h1>
+        <div className={classes.technologyContainer}>
+            <div className={technologyContainer}>
+            <div className={classes.content}>
+                <div className={classes.text}>
+                    <h1><span>03</span>SPACE LAUNCH 101</h1>
+                    <h2>THE TERMINOLOGY…</h2>
+                    <h3>{technologyName}</h3>
+                    <p>{technologyDescription}</p>
+                </div>
+                <div className={classes.slider}>
+                    <span 
+                        onClick={() => HandleActive(0)}
+                        className={active === 0 ? classes.active : undefined}
+                        ></span>
+                    <span 
+                        onClick={() => HandleActive(1)}
+                        className={active === 1 ? classes.active : undefined}
+                        ></span>
+                    <span 
+                        onClick={() => HandleActive(2)}
+                        className={active === 2 ? classes.active : undefined}
+                        ></span>
+                </div>
+            </div>
+                <div className={classes.technologyImg}></div>
+            </div>
+        </div>
      );
 }
  
